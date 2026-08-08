39a2c80b449f8da2ca2a4face94c938b18397c98
Andrey <susluk93@mail.ru>
2026-08-08 08:33:51 +0300

    Updated Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -113,25 +113,6 @@ const Layout = () => {
               MY_JOURNEY
             </Typography>
 
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: 'flex', md: 'none' },
-                flexGrow: 1,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              MY_JOURNEY
-            </Typography>
-
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} />
 
             {/* Аватар / Кнопка пользователя */}
